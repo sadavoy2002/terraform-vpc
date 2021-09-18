ad221eab5985e57af9d76ea458213219e5224504
Mukhammadaziz12 <89210554+Mukhammadaziz12@users.noreply.github.com>
2021-09-18 14:19:19 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -5,3 +5,20 @@ terraform {
         region = "us-east-1"
     }
 }
+
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "s3:ListBucket",
+      "Resource": "arn:aws:s3:::mybucket"
+    },
+    {
+      "Effect": "Allow",
+      "Action": ["s3:GetObject", "s3:PutObject"],
+      "Resource": "arn:aws:s3:::mybucket/path/to/my/key"
+    }
+  ]
+}
+
