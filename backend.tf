d63d3738b827578b8a28321b8ee39179b3708e50
Mukhammadaziz12 <89210554+Mukhammadaziz12@users.noreply.github.com>
2021-09-18 14:14:50 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" {
-        bucket = "bucket-terraform1"
+        bucket = "terraform-bucke"
         key = "terraform_vpc/us-east-1/class/dev/infrastructure.tfstate"
         region = "us-east-1"
     }
